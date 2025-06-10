34a2aa6bc12cf33496934cb8a1023afb4f480782
sileysa <sileysafaedatulnuraini984@gmail.com>
2025-06-10 11:26:18 +0700

    praktikum14

---
+++ Praktikum14/Customer.java
@@ -0,0 +1,18 @@
+public class Customer {
+
+    public int id;
+    public String name;
+
+    public Customer() {
+
+    }
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return " ID: " + this.id + " Nama: " + this.name;
+    }
+}
---
+++ Praktikum14/TreeSetDemo.java
@@ -0,0 +1,31 @@
+import java.util.TreeSet;
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet<String> fruits = new TreeSet<>();
+
+        fruits.add("Mangga");
+        fruits.add("Apel");
+        fruits.add("Jeruk");
+        fruits.add("Jambu");
+
+        for (String temp : fruits) {
+            System.out.println(temp);
+        }
+
+        for (String temp : fruits) {
+            System.out.println(temp);
+        }
+
+        System.out.println("First: " + fruits.first());
+        System.out.println("Last: " + fruits.last());
+
+        fruits.remove("Jeruk");
+        System.out.println("Setelah remove " + fruits);
+
+        fruits.pollFirst();
+        System.out.println("Setelah poll first " + fruits);
+
+        fruits.pollLast();
+        System.out.println("Setelah poll last " + fruits);
+    }
+}
